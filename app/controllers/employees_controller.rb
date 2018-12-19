058bbaa8e9b78caff6bafad744cb0e5d385a5f7b
atkinsonholly <atkinsonholly@gmail.com>
2018-12-19 15:17:17 +0000

    Done

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,43 @@
 class EmployeesController < ApplicationController
+
+  def index
+    @employees = Employee.all
+  end
+
+  def new
+    @employee = Employee.new
+    @dogs = Dog.all
+  end
+
+  def create
+    @employee = Employee.create(employee_params)
+    if @employee.valid?
+      redirect_to employees_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @employee = Employee.find(params[:id])
+    @dogs = Dog.all
+  end
+
+  def update
+    @employee = Employee.find(params[:id])
+    @employee.update(employee_params)
+    redirect_to employee_path(@employee)
+  end
+
+
+  def show
+    @employee = Employee.find(params[:id])
+  end
+
+  private
+
+  def employee_params
+    params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :dog, :img_url, :dog_id)
+    #Had to add dog_id because of the way the form works!
+  end
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,11 @@
 class Employee < ApplicationRecord
+  belongs_to :dog
+
+  validates :alias, uniqueness: true
+  validates :title, uniqueness: true
+
+  def name_to_string
+    first_name.to_s + " " + last_name.to_s
+  end
+
 end
