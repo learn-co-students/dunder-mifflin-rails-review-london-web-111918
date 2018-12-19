70632f1470a3dfd7f5ed4427771ea5b63c42bf4d
IMH51 <ianmartinharrison@outlook.com>
2018-12-19 16:03:11 +0000

    created dunder mifflin dog app

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,15 @@
 class Employee < ApplicationRecord
+  belongs_to :dog
+
+  validates :alias, uniqueness: true, if: :no_alias?
+  validates :title, uniqueness: true, if: :no_title?
+
+  def no_alias?
+    :alias == "none"
+  end
+
+  def no_title?
+    :title == "none"
+  end
+
 end
